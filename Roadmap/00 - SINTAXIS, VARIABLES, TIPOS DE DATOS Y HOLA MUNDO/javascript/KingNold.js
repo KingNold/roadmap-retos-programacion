4f14a56d4baa1c3b8e91a5210eb59a7a13d0a723
KingNold <reinol_osvaldo@hotmail.com>
2025-08-30 01:41:46 -0600

    kingNold

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/KingNold.js
@@ -0,0 +1,27 @@
+// No tiene un sitio web oficial como tal
+//https://ecma-international.org/publications-and-standards/standards/ecma-262/
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//comentarios: los comentarios en JavaScript se coloca con dobe slash //
+/* con este 
+   se puede comentar 
+   en varias lineas
+*/
+
+//una variable en JavaScript se declara con la palara reservada let 
+let firstName = "Nombre";
+//tambien se puede declarar constantes con la palabra reservada const
+const gravity = 9.8;
+
+//tipos de datos
+let tipoTexto = "Texto"; //string
+let tipoNumeroEntero = 20; //number
+let tipoNumeroFlotante = 3.1416; //float
+let tipoBigInt = 1234567890; // un numero muy grande
+let tipoEsBoleano = true; //boolean true o false
+let tipoNulo = null; //nulo
+let tipoIndefinido; //undefine no tiene variable definida
+let tipoSimbolo = Symbol('id_unico'); //Crea identificadores únicos e inmutables
+
+//mensaje por terminal
+console.log("Hola, JavaScript");
